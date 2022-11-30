6a3b798fd6b124f3a892ba700f7b39890e918c7e
Mariia <bolotina.23reg@yandex.ru>
2022-11-30 22:15:14 +0300

    Добавили #2

---
+++ HW2/2/Program.cs
@@ -0,0 +1,14 @@
+﻿// задание 2
+
+// задание 1
+
+int Number(int num)
+{
+    Console.WriteLine(num);
+var str = num.ToString();
+num = int.Parse(str.Remove(str.Length - 2, 1));
+return (num);
+}
+
+Console.WriteLine(Number (new Random().Next(100, 999)));
+
